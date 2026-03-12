42cf6269605502719674edfc65a2503a039166d3
Mansourngom <mansourngom657@gmail.com>
2026-03-12 09:59:52 +0000

    Derniere modifications

--- api/views.py
+++ api/views.py
@@ -2,7 +2,7 @@ from rest_framework import generics, permissions, status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from django.contrib.auth.models import User
-from .models import Profile, Event, Registration
+from .models import Event, Registration
 from .serializers import RegisterSerializer, UserSerializer, EventSerializer, RegistrationSerializer
 
 
